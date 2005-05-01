679ff37c5bf51effbcd700940f0e49a0e04042b0
zelon <devnull@localhost>
2005-05-01 10:10:45 +0000

    파일명이나 폴더명을 비교할 때(정렬할때쓰임) 모두 소문자로 변환해서 비교

--- ZViewer/lib/CommonFunc.cpp
+++ ZViewer/lib/CommonFunc.cpp
@@ -21,8 +21,14 @@ void DebugPrintf( const char *fmt, ... )
 
 bool StringCompare(const std::string & a, const std::string & b)
 {
-#pragma message("TODO : 문자열 비교를 할 때 모두 소문자로 바꿔서 비교해야함")
-	return (strcmp(b.c_str(), a.c_str()) > 0);
+	// 문자열 비교를 할 때 모두 소문자로 바꿔서 비교한다.
+	char szTempA[FILENAME_MAX], szTempB[FILENAME_MAX];
+	_snprintf(szTempA, sizeof(szTempA), a.c_str());
+	_snprintf(szTempB, sizeof(szTempB), b.c_str());
+	strlwr(szTempA);
+	strlwr(szTempB);
+
+	return (strcmp(szTempA, szTempB) > 0);
 }
 
 const std::string GetOnlyFileName(const std::string & strFullFileName)
@@ -35,7 +41,14 @@ const std::string GetOnlyFileName(const std::string & strFullFileName)
 
 bool FilenameCompare(const std::string & a, const std::string & b)
 {
-	return (strcmp(GetOnlyFileName(b).c_str(), GetOnlyFileName(a).c_str()) > 0);
+	// 파일명 비교를 할 때 모두 소문자로 바꿔서 비교한다.
+	char szTempA[FILENAME_MAX], szTempB[FILENAME_MAX];
+	_snprintf(szTempA, sizeof(szTempA), GetOnlyFileName(a).c_str());
+	_snprintf(szTempB, sizeof(szTempB), GetOnlyFileName(b).c_str());
+	strlwr(szTempA);
+	strlwr(szTempB);
+
+	return (strcmp(szTempB, szTempA) > 0);
 }
 
 
