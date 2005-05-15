e3d948965cba2e832cae943eb9816b4916882ded
zelon <devnull@localhost>
2005-05-15 14:35:47 +0000

    *** empty log message ***

--- ZViewer/LangKor/res/resource.h
+++ ZViewer/LangKor/res/resource.h
@@ -67,13 +67,14 @@
 #define ID_SORT_FILESIZE                40077
 #define ID_SORT_FILEDATE                40078
 #define ID_FILE_RESCAN_FOLDER           40080
+#define ID_SHOW_CACHED_FILENAME         40081
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        115
-#define _APS_NEXT_COMMAND_VALUE         40081
+#define _APS_NEXT_COMMAND_VALUE         40083
 #define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
