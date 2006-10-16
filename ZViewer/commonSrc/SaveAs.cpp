b1dce4965378f6a221241b7e68c6076093514a56
zelon <devnull@localhost>
2006-10-16 17:04:06 +0000

    SaveAs dialog 를 제위치에 띄우려고 시도 중.

--- ZViewer/commonSrc/SaveAs.cpp
+++ ZViewer/commonSrc/SaveAs.cpp
@@ -27,6 +27,44 @@ CSaveAs::~CSaveAs()
 {
 }
 
+/// 파일 열기 다이얼로그는 항상 부모의 가운데에 띄운다.
+UINT_PTR CALLBACK OFNHookProc(HWND hdlg, UINT uiMsg, WPARAM wParam, LPARAM lParam )
+{
+	switch ( uiMsg )
+	{
+	case WM_INITDIALOG:
+		{
+			HWND hFileOpenHWND = ::GetParent(hdlg);
+			HWND hParentHWND = ::GetParent(hFileOpenHWND);
+			RECT thisRect, parentRect;
+			if ( ::GetWindowRect(hParentHWND, &parentRect) )
+			{
+				if ( ::GetWindowRect(hFileOpenHWND, &thisRect) )
+				{
+					int centerX = (parentRect.right - parentRect.left) / 2;
+					int centerY = (parentRect.bottom - parentRect.top ) / 2;
+
+					int toX = centerX - (thisRect.right - thisRect.left ) / 2;
+					int toY = centerY - (thisRect.bottom - thisRect.top ) / 2;
+
+					::MoveWindow(hFileOpenHWND, toX, toY, thisRect.right - thisRect.left, thisRect.bottom - thisRect.top, TRUE);
+					return 1;
+				}
+				else
+				{
+					_ASSERTE(false);
+				}
+			}
+			else
+			{
+				_ASSERTE(false);
+			}
+		}
+		break;
+	}
+	return 0L;
+}
+
 /// 다른 이름으로 저장창을 띄운다. 반환값이 false 이면 저장하지 않는다.
 bool CSaveAs::showDialog()
 {
@@ -47,7 +85,19 @@ bool CSaveAs::showDialog()
 	m_ofn.lpstrFileTitle = NULL;
 	m_ofn.nMaxFileTitle = 0;
 	m_ofn.lpstrInitialDir = m_strInitialiFolder.c_str();
-	m_ofn.Flags = OFN_OVERWRITEPROMPT;
+	
+	bool bUseHook = false;
+
+	if ( bUseHook )
+	{
+		m_ofn.Flags = OFN_OVERWRITEPROMPT | OFN_ENABLEHOOK | OFN_EXPLORER | OFN_ENABLESIZING | OFN_SHOWHELP;
+		m_ofn.lpfnHook = OFNHookProc;
+	}
+	else
+	{
+		m_ofn.Flags = OFN_OVERWRITEPROMPT;// | OFN_ENABLEHOOK;
+		//m_ofn.lpfnHook = OFNHookProc;
+	}
 
 	if ( FALSE == GetSaveFileName(&m_ofn) )
 	{
