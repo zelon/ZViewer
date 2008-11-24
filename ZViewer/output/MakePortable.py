ab76aa73e4e493863dcc68f10aff86ae7de6ff59
zelon <devnull@localhost>
2008-11-24 22:48:23 +0000

    Portable 버젼 만들기 추가.

---
+++ ZViewer/output/MakePortable.py
@@ -0,0 +1,48 @@
+import re
+import os
+
+def getCurrentVersionFromNSIS():
+	
+	file = open(r"ZViewer.nsi")
+    
+	lines = file.readlines()
+	file.close()
+    
+	outputs = []    
+	for i in lines:
+    	
+		m = re.match(r"!define\sPRODUCT_VERSION\s\"(.*)\"", i)
+    	
+		if m != None:
+			"""outputs.append("!define PRODUCT_VERSION \"" + str(major) + "." + str(minor) + "." + str(patch) + str(postfix) + "\"\n")"""
+			return (m.group(1))
+		else:
+			pass
+	return "Cannot get current version from nsis"
+
+def testmain():
+	strCurrentVersion = getCurrentVersionFromNSIS()
+	str7ZipPath = r"C:\Program Files\7-Zip\7z.exe" 
+	if True == os.path.isfile(str7ZipPath):
+		print("Founded 7z")
+	else:
+		print("[FAILED] There is no 7zip : " + str7ZipPath)
+		return 1
+
+	files = ["language", "_Zviewer.png", "FreeImage.dll", "FreeImagePlus.dll", "License.txt", "LoadError.png", "ReadMe.txt", "ZViewer.exe", "ZViewerAgent.dll", "ZViewerIcons.dll"]
+
+	for file in files:
+		if False == os.path.isfile(file) and False == os.path.isdir(file):
+			print("[FAILED] Cannot find " + file)
+			return 1
+
+	strCmd = "\"" + str7ZipPath + "\" a ZViewerPortable" + strCurrentVersion + ".zip "
+	for file in files:
+		strCmd = strCmd + file + " "
+
+	print strCmd
+	os.system(strCmd)
+
+if __name__ == "__main__":
+	testmain()
+	os.system("pause")
