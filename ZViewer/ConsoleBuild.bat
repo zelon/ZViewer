abdcb2cbb95af9e861385758e858d4fe7e46de95
zelon <devnull@localhost>
2008-12-12 22:34:03 +0000

    add build argument (rebuild,clean) like makefile

--- ZViewer/ConsoleBuild.bat
+++ ZViewer/ConsoleBuild.bat
@@ -1,2 +1,17 @@
+@echo off
 call "c:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
-devenv ZViewerWin32.sln /build Debug
+IF "%1" == "rebuild" GOTO REBUILD
+IF "%1" == "clean" GOTO CLEAN
+devenv ZViewerWin32.sln /build ZViewerDebug
+GOTO END
+
+:REBUILD
+devenv ZViewerWin32.sln /rebuild ZViewerDebug
+GOTO END
+
+:CLEAN
+devenv ZViewerWin32.sln /clean ZViewerDebug
+GOTO END
+
+:END
+
